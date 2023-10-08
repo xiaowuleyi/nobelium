e63634596c563f08631638ce2b8b64151d26bdcc
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2023-10-08 20:15:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,22 +1,22 @@
 const BLOG = {
-  title: 'FAMEBNB', 
-  author: '彩荣客栈', 
-  email: 'w@famebnb.com', 
+  title: '小吴乐意blog',  
+  author: '小吴', 
+  email: 'xiaowuleyi@gmail.com',  
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.', 
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES'] 
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc 
+  darkBackground: '#18181B', // use hex value, don't forget '#' 
   path: '', // leave this empty unless you want to deploy Nobelium in a folder 
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar 
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/craigaryhart',
   seo: {
