05eb8dfef95f42bec9522a0a3eb43c16e2696edf
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2023-10-08 07:37:01 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
   title: '小吴乐意blog',  
   author: '小吴', 
-  email: 'xiaowuleyi@gmail.com',   
+  email: '0471666@gmail.com',    
   link: 'https://xiaowuleyi.com',
-  description: 'This gonna be an awesome website.',  
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']  
+  description: 'This gonna be an awesome website.',   
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']   
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif'] 
