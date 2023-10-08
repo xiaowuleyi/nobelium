e70ee533392a1691bfc9b08482892fe8d25cc11c
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2023-10-08 07:29:15 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,13 +1,13 @@
 const BLOG = {
   title: '小吴乐意blog',  
   author: '小吴', 
-  email: 'xiaowuleyi@gmail.com',  
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.', 
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES'] 
+  email: 'xiaowuleyi@gmail.com',   
+  link: 'https://xiaowuleyi.com',
+  description: 'This gonna be an awesome website.',  
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']  
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
+  font: 'sans-serif', // ['sans-serif', 'serif'] 
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc 
   darkBackground: '#18181B', // use hex value, don't forget '#' 
   path: '', // leave this empty unless you want to deploy Nobelium in a folder 
