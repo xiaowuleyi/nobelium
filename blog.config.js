4813f798dd8410d029c6d77927244376eda9d995
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2023-10-08 08:32:39 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,34 +3,34 @@ const BLOG = {
   author: '小吴', 
   email: '0471666@gmail.com',     
   link: 'https://xiaowuleyi.com',
-  description: 'This gonna be an awesome website.',   
+  description: '过着本分的生活，赚着踏实的钱，菲律宾生活记录中……',    
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']   
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'], 
-  font: 'sans-serif', // ['sans-serif', 'serif'] 
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc  
-  darkBackground: '#18181B', // use hex value, don't forget '#' 
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder  
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
-  sortByDate: false,
-  showAbout: true,
-  showArchive: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar 
+  appearance: 'auto', // ['light', 'dark', 'auto'],  
+  font: 'sans-serif', // ['sans-serif', 'serif']  
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc   
+  darkBackground: '#18181B', // use hex value, don't forget '#'  
+  path: '', // leave this empty unless you want to deploy Nobelium in a folder   
+  since: 2023, // If leave this empty, current year will be used. 
+  postsPerPage: 9, 
+  sortByDate: false, 
+  showAbout: true, 
+  showArchive: true, 
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar  
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/xiaowuleyi',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code 
+    keywords: ['菲律宾', '小吴乐意', '博客', '独立博客', '比特币'], 
+    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code  
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
-  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it. 
-    ackeeConfig: {
+  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！ 
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public 
+  analytics: { 
+    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.  
+    ackeeConfig: { 
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b' 
     },
     gaConfig: {
       measurementId: '' // e.g: G-XXXXXXXXXX
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis 
-    provider: 'cusdis', // leave it empty if you don't need any comment plugin 
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin  
     gitalkConfig: { 
       repo: '', // The repository of store comments 
       owner: '',
