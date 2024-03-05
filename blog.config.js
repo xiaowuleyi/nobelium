f5bb9c583cbd75f734f90dd94a6b357fa5048b53
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-05 10:22:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,14 +26,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！ 
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public 
   analytics: { 
-    provider: 'G-L0ZYWHJPC3', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.   
+    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.   
     ackeeConfig: { 
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b' 
     },
     gaConfig: {
-      measurementId: 'G-L0ZYWHJPC3' // e.g: G-XXXXXXXXXX 
+      measurementId: '' // e.g: G-XXXXXXXXXX 
     }
   },
   comment: {
