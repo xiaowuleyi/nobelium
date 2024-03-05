298e841dd4814236879306d81306a780396aa56c
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-05 14:42:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: 'b161792d-0482-4338-b523-e488bd3c8c73', // data-app-id 
+      appId: '', // data-app-id 
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
